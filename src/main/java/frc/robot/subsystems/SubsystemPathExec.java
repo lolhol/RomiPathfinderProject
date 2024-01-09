5e8e709b31e0a9711a52dd522428ad08601632aa
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-01-08 18:54:04 -0800

    .

--- src/main/java/frc/robot/subsystems/SubsystemPathExec.java
+++ src/main/java/frc/robot/subsystems/SubsystemPathExec.java
@@ -134,7 +134,7 @@ public class SubsystemPathExec extends SubsystemBase {
         widgetRobot.setString(
             "a=" +
                 Math.round(
-                    MathUtil.normaliseDeg(Math.toDegrees(curPosData[2]))));
+                    Math.toDegrees(curPosData[2])));
         widgetTarget.setString(
             "a=" +
                 Math.round(angleDiff));
